adbe0753820845f31cab2ec8cff6fbe029347926
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-09-13 10:03:32 -0500

    testing the shirly account

--- src/app/home/components/Footer.tsx
+++ src/app/home/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from '../styles/Footer.module.css';
 
 const Footer: React.FC = () => (
-  <footer className={styles.footer}>ʕ•ᴥ•ʔ jhey &copy; 202</footer>
+  <footer className={styles.footer}>ʕ•ᴥ•ʔ jhey &copy; 2024</footer>
 );
 
 export default Footer;
